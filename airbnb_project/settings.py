f7700a73f1e244705b993a8daa295fba9cce65e2
thiggimajig <higgins.taylor.a@gmail.com>
2022-07-18 19:13:08 -0400

    adding settings.py where i commented out secret key

--- airbnb_project/settings.py
+++ airbnb_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-==ti$gj(=8h&_sdji)n!@s@)6kj8i^sw1u1=vi8b)mxd&7gz3l'
+# SECRET_KEY = 'django-insecure-==ti$gj(=8h&_sdji)n!@s@)6kj8i^sw1u1=vi8b)mxd&7gz3l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
