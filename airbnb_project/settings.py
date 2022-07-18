9929df1d2ac541549ccca8c83db55096ef9c593e
thiggimajig <higgins.taylor.a@gmail.com>
2022-07-18 19:47:08 -0400

    deleted old secret key

--- airbnb_project/settings.py
+++ airbnb_project/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-==ti$gj(=8h&_sdji)n!@s@)6kj8i^sw1u1=vi8b)mxd&7gz3l'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
