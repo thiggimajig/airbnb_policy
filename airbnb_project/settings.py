bd521431c6dc68b2fc011495316723f95c70eb25
thiggimajig <higgins.taylor.a@gmail.com>
2022-07-22 12:49:19 +0200

    adding airbnb_app to installed apps in settings.py

--- airbnb_project/settings.py
+++ airbnb_project/settings.py
@@ -34,7 +34,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'csvimport.app.CSVImportConf'
+    'csvimport.app.CSVImportConf',
+    'airbnb_app'
 ]
 
 MIDDLEWARE = [
