1f61521d64c30815a91ee21e3a1a6af1ee1050c7
thiggimajig <higgins.taylor.a@gmail.com>
2022-07-28 11:43:25 +0200

    adding empty test file to re do heroku app

--- airbnb_app/tests.py
+++ airbnb_app/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
+# a separate TestClass for each model or view
+# a separate test method for each set of conditions you want to test
+# test method names that describe their function
+# Django includes LiveServerTestCase to facilitate integration with tools like Selenium.
 # Create your tests here.
+#example here 
+# class QuestionModelTests(TestCase):
+
+#     def test_was_published_recently_with_future_question(self):
+#         """
+#         was_published_recently() returns False for questions whose pub_date
+#         is in the future.
+#         """
+#         time = timezone.now() + datetime.timedelta(days=30)
+#         future_question = Question(pub_date=time)
+#         self.assertIs(future_question.was_published_recently(), False)
