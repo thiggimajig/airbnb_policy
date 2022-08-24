75eafa87a2f311a8db8f49a7305fb7d6ec7f4f1f
thiggimajig <higgins.taylor.a@gmail.com>
2022-08-24 22:10:35 +0200

    okkay so we added whitenoise compression stati manifest in settings to handle compression instead of gzipping manually and calling it in templates, works in dev and is a good deal smaller files and definielty gzipped so thats cool just not much faster but maybe ones its cacached it'll be faster

--- airbnb_project/settings.py
+++ airbnb_project/settings.py
@@ -164,6 +164,8 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR + '/staticfiles'
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
